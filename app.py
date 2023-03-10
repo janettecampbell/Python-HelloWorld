4e6fba76de665a8b1580745c087a286b96742e32
Janette Campbell <campbjp@hotmail.com>
2023-03-10 15:06:48 -0500

    Lesson 9

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
-import locale
-
-house_price = 1000000
+has_high_income = True
 has_good_credit = True
+has_criminal_record = False
 
-if has_good_credit:
-    percent_down = 10
-else:
-    percent_down = 20
-
-down_payment = house_price * (percent_down/100)
-locale.setlocale(locale.LC_ALL, "en_US")
-down_payment_formatted = locale.currency(down_payment, symbol=True, grouping=True, international=False)
-
-print(f"Your down payment is {percent_down}%. Which comes to {down_payment_formatted}.")
+if has_high_income or has_good_credit and not has_criminal_record:
+    print("Eligible for loan")

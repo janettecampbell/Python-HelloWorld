6a787a5396aa1ed3b062ae3142142d0aae2dc42c
Janette Campbell <campbjp@hotmail.com>
2023-03-13 14:46:29 -0400

    Lesson 21

--- app.py
+++ app.py
@@ -1,11 +1,48 @@
-# Dictionaries
+# Convert Digits to Words
 
-customer = {
-    "name": "John Smith",
-    "age": 30,
-    "is_verified": True
+phone_number = input("Phone: ")
+number_words = ""
+
+for number in phone_number:
+    if number == "1":
+        number_words += "one "
+    if number == "2":
+        number_words += "two "
+    if number == "3":
+        number_words += "three "
+    if number == "4":
+        number_words += "four "
+    if number == "5":
+        number_words += "five "
+    if number == "6":
+        number_words += "six "
+    if number == "7":
+        number_words += "seven "
+    if number == "8":
+        number_words += "eight "
+    if number == "9":
+        number_words += "nine "
+    if number == "0":
+        number_words += "zero "
+print(number_words)
+
+#OR
+
+digits_mapping = {
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine",
+    "0": "zero"
 }
+output_str = ""
 
-customer["birthdate"] = "Jan 1 1980"
+for character in phone_number:
+    output_str += digits_mapping.get(character, "!") + " "
 
-print(customer["birthdate"])
+print(output_str)

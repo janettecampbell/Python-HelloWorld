61ff04f6cb34160eb29486eb487df84d70323d38
Janette Campbell <campbjp@hotmail.com>
2023-03-13 17:37:26 -0400

    Lesson 34

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-# Generating Random Numbers
+# Pathlib
 
-import random
+from pathlib import Path
 
+path = Path()
 
-class Dice:
-    def roll(self):
-        random_x = random.randint(1, 6)
-        random_y = random.randint(1, 6)
-        return random_x, random_y
-
-
-roll1 = Dice()
-
-print(roll1.roll())
+for file in path.glob("*.py"):
+    print(file)

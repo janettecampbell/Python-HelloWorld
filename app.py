c6815b772a8592f7901603155ebccfb31422b251
Janette Campbell <campbjp@hotmail.com>
2023-03-13 13:41:28 -0400

    Ex-14

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-numbers = [5, 2, 5, 2, 2]
+numbers_F = [5, 2, 5, 2, 2]
+numbers_L = [2, 2, 2, 2, 5]
 
-for number in numbers:
+for number in numbers_L:
     string = ""
     for item in range(number):
         string += "X"

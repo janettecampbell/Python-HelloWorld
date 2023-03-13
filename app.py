172ed646274c97cf37c6bfef984761c1045b94a1
Janette Campbell <campbjp@hotmail.com>
2023-03-13 16:16:22 -0400

    Lesson 28

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 # Classes
 
 class Point:
-    def move(selfs):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self):
         print("move")
 
     def draw(self):
         print("draw")
 
 
-point1 = Point()
-point1.x = 10
-point1.y = 20
-point1.draw()
-
-print(point1.x)
-
-point2 = Point()
+point = Point(10, 20)
+point.x = 11
+print(point.x)

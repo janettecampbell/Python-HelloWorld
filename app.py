285b4b1760d39a5fe9e62ac125d0bcf428c04611
Janette Campbell <campbjp@hotmail.com>
2023-03-13 14:27:05 -0400

    Lesson 20

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-# Unpacking
+# Dictionaries
 
-coordinates = (1, 2, 3)
-x, y , z = coordinates
+customer = {
+    "name": "John Smith",
+    "age": 30,
+    "is_verified": True
+}
 
+customer["birthdate"] = "Jan 1 1980"
+
+print(customer["birthdate"])

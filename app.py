c5816b33ad3f14d7b46de68d1a765c02ef54a9a2
Janette Campbell <campbjp@hotmail.com>
2023-03-13 14:54:44 -0400

    Lesson 22

--- app.py
+++ app.py
@@ -1,48 +1,15 @@
-# Convert Digits to Words
+# Emoji Converter
 
-phone_number = input("Phone: ")
-number_words = ""
-
-for number in phone_number:
-    if number == "1":
-        number_words += "one "
-    if number == "2":
-        number_words += "two "
-    if number == "3":
-        number_words += "three "
-    if number == "4":
-        number_words += "four "
-    if number == "5":
-        number_words += "five "
-    if number == "6":
-        number_words += "six "
-    if number == "7":
-        number_words += "seven "
-    if number == "8":
-        number_words += "eight "
-    if number == "9":
-        number_words += "nine "
-    if number == "0":
-        number_words += "zero "
-print(number_words)
-
-#OR
-
-digits_mapping = {
-    "1": "one",
-    "2": "two",
-    "3": "three",
-    "4": "four",
-    "5": "five",
-    "6": "six",
-    "7": "seven",
-    "8": "eight",
-    "9": "nine",
-    "0": "zero"
+message = input(">")
+words = message.split(" ")
+emojis = {
+    ":)": "😀",
+    ":(": "😔"
 }
-output_str = ""
 
-for character in phone_number:
-    output_str += digits_mapping.get(character, "!") + " "
+output = ""
 
-print(output_str)
+for word in words:
+    output+= emojis.get(word, word) + " "
+
+print(output)

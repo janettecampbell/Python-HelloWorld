e315284953ec1e3dc0486a46b6ca17458697a853
Janette Campbell <campbjp@hotmail.com>
2023-03-13 18:19:53 -0400

    Lesson 35

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-# Pathlib
+import openpyxl as xl
 
-from pathlib import Path
+wb = xl.load_workbook("transactions.xlsx")
+sheet = wb["Sheet1"]
+cell = sheet["a1"]
+cell = sheet.cell(1,1)
 
-path = Path()
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    corrected_price = cell.value * 0.9
+    corrected_price_cell = sheet.cell(row,4)
+    corrected_price_cell.value = corrected_price
 
-for file in path.glob("*.py"):
-    print(file)
+wb.save("transactions2.xlsx")

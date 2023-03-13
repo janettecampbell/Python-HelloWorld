452df057932f9864c553192ec8e7902a09f93efc
Janette Campbell <campbjp@hotmail.com>
2023-03-13 13:40:15 -0400

    Ex-14

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
-for x in range(4):
-    for y in range(3):
-        print(f"({x}, {y})")
+numbers = [5, 2, 5, 2, 2]
+
+for number in numbers:
+    string = ""
+    for item in range(number):
+        string += "X"
+    print(string)

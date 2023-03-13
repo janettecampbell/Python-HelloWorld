37a2f47ae41d7a2099669845daeb750bcfee5aaa
Janette Campbell <campbjp@hotmail.com>
2023-03-13 17:21:02 -0400

    Lesson 33

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-# Packages
+# Generating Random Numbers
 
-from ecommerce import shipping
+import random
 
-shipping.calc_shipping()
+
+class Dice:
+    def roll(self):
+        random_x = random.randint(1, 6)
+        random_y = random.randint(1, 6)
+        return random_x, random_y
+
+
+roll1 = Dice()
+
+print(roll1.roll())

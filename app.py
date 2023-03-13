f79e230831402f4d77185b3f7364cfe1ffbe8550
Janette Campbell <campbjp@hotmail.com>
2023-03-13 14:11:25 -0400

    Ex-17

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-numbers = [5, 2, 1, 5, 7, 4]
-numbers2 = numbers.copy()
-numbers.append(10)
+numbers = [2, 2, 4, 6, 3, 4, 6, 1]
 
-print(numbers)
-print(numbers2)
+# Delete Duplicates
+uniques = []
+
+for number in numbers:
+    if number not in uniques:
+        uniques.append(number)
+
+print(uniques)
